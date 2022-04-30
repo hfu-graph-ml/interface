c8a7a3da9c55c6b2bd1d31886d4a8803b4756c67
Techassi <git@techassi.dev>
2022-04-30 22:33:22 +0200

    Add 'wait_or_exit' func to utils

---
+++ src/utils/wait.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+
+def wait_or_exit(d: int, key: str = 'q') -> bool:
+    # Fallback to 16 ms => 60 FPS
+    if d <= 0:
+        d = 16
+
+    code = cv.waitKey(d)
+    if code == ord(key):
+        return True
+
+    return False
