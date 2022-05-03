24ccb6306b9a8cc983fee37e038d90c4c68df007
Techassi <git@techassi.dev>
2022-05-03 20:13:03 +0200

    Update 'track' command

--- src/cmd/track.py
+++ src/cmd/track.py
@@ -1,5 +1,6 @@
 import click
 
+from renderer.renderer import Renderer
 from tracker.tracker import Tracker
 import config.config as config
 
@@ -11,11 +12,19 @@ def execute(config_path: str, camera_id: int):
         click.echo(f'Error while reading config: {err.message}')
         return
 
+    # Create tracker
     t = Tracker(cfg['tracker'])
-    err = t.run_threaded(camera_id, 60)
+    err = t.start(camera_id, 60)
     if err != None:
         click.echo(err.message)
 
     click.echo('Tracking running...')
+
+    # Create renderer
+    click.echo('Rendering running...')
+    r = Renderer(cfg['renderer'], t)
+    err = r.start()
+    if err != None:
+        click.echo(err.message)
 
     # TODO (Techassi): Handle interupts and call t.stop()
