faa6dd0efd5f89a91c5b7016ab20f8f3e3a91cc4
Techassi <git@techassi.dev>
2022-05-06 15:54:26 +0200

    First renderer output

--- src/renderer/renderer.py
+++ src/renderer/renderer.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import numpy as np
 import cv2 as cv
 import math
@@ -9,18 +8,33 @@ import utils.colors as colors
 import utils.wait as wait
 
 from .types import RendererError
+from .shared import Shared
 
 
-class Renderer:
+class Renderer(Shared):
     '''
     This class describes the renderer which renders the user interface.
     '''
 
-    def __init__(self, cfg: config.CaptureOptions, t: Tracker) -> None:
+    def __init__(self, cfg: config.Config, t: Tracker) -> None:
+        self._fps = cfg['capture']['fps']
         self._subscription_id = -1
-        self._fps = cfg['fps']
         self._running = False
         self._tracker = t
+
+        # Dimensions
+        self._frame_height = cfg['renderer']['height']
+        self._frame_width = cfg['renderer']['width']
+
+        # Camera dimensions
+        self._camera_frame_height = 0
+        self._camera_frame_width = 0
+
+        # Scaling
+        self._scaling_factor = 1
+
+    def _calibrate():
+        ''''''
 
     def start(self) -> RendererError:
         '''
@@ -37,11 +51,22 @@ class Renderer:
         cv.namedWindow(window_name)
 
         # Subscribe to the tracker
-        id, retrieve = self._tracker.subscribe()
+        id, params, retrieve = self._tracker.subscribe()
         self._subscription_id = id
 
         while self._running:
-            frame = np.zeros((500, 500, 1))
+            frame = np.zeros((self._frame_height, self._frame_width, 3))
+
+            try:
+                corners, ids = retrieve(False)
+
+                for i, corners_per_marker in enumerate(corners):
+                    if len(corners_per_marker[0]) != 4:
+                        continue
+
+                    self._draw_center_point(corners_per_marker[0], ids[i], frame)
+            except:
+                pass
 
             cv.imshow(window_name, frame)
 
@@ -49,12 +74,14 @@ class Renderer:
                 break
 
         # Cleanup
-        cv.destroyAllWindows()
+        self.stop()
         return None
 
     def stop(self):
         '''
         Stop the render loop.
         '''
+        cv.destroyAllWindows()
+
         self._tracker.unsubscribe(self._subscription_id)
-        self._running = False
+        self._tracker.stop()
